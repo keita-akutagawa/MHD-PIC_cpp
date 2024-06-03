87fb6811a55b3c7a009f46707932354263edf6dd
Keita <keita19990517@gmail.com>
2024-06-03 10:16:23 +0900

    20240603

---
+++ examples_1D/alfven/main_alfven.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include "../../lib_mhdpic_1D/MHDPIC1D.hpp"
+
+
+
+void MHDPIC1D::initialize()
+{
+    
+}
+
+
+int main()
+{
+    
+
+    return 0;
+}
+
+
+
